928d26ce8402704ae46623c2e8acad2ed25e3a1c
tinden <dummy.email@bk.ru>
2019-01-11 18:16:59 +0300

    Add missing telegram bot models

---
+++ QuickCourses.TelegramBot.Models/CourseState.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickCourses.TelegramBot.Models
+{
+    public class CourseState
+    {
+        public string ProgressId { get; set; }
+        public string CourseId { get; set; }
+        public int StepId { get; set; }
+        public int LessonId { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
